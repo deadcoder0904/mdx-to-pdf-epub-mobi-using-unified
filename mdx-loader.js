67467347e23782df72ee626bbc43f916c95b3215
Akshay Kadam <akshaykadam0904@gmail.com>
2022-10-10 19:50:17 +0530

    unwrap image from p

--- mdx-loader.js
+++ mdx-loader.js
@@ -1,11 +1,12 @@
 import { createLoader } from '@mdx-js/node-loader'
 import remarkGfm from 'remark-gfm'
 import remarkFrontmatter from 'remark-frontmatter'
+import remarkUnwrapImages from 'remark-unwrap-images'
 
 // Load is for Node 17+, the rest for 12, 14, 16.
 const { load, getFormat, transformSource } = createLoader({
   fixRuntimeWithoutExportMap: false,
-  remarkPlugins: [remarkGfm, remarkFrontmatter],
+  remarkPlugins: [remarkGfm, remarkFrontmatter, remarkUnwrapImages],
 })
 
 export { load }
