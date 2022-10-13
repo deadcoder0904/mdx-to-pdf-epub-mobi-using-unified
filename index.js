5a6232fdd29bc99ac2a49b78baa63776a995f299
Akshay Kadam <akshaykadam0904@gmail.com>
2022-10-13 19:42:10 +0530

    add cover image to the top of body

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ import rehypeParse from 'rehype-parse'
 import rehypeStringify from 'rehype-stringify'
 import rehypeDocument from 'rehype-document'
 import rehypeFormat from 'rehype-format'
+import rehypeRewrite from 'rehype-rewrite'
 import rehypeAddClasses from 'rehype-add-classes'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 
@@ -62,6 +63,23 @@ const main = async () => {
       title: meta.title || 'book',
       css: 'styles/pdf.css',
     }) // document should be after sanitize
+    .use(rehypeRewrite, {
+      rewrite: (node) => {
+        if (node.type == 'element' && node.tagName == 'body') {
+          node.children = [
+            {
+              type: 'element',
+              tagName: 'img',
+              properties: {
+                src: meta.cover || '',
+                alt: 'cover',
+              },
+            },
+            ...node.children,
+          ]
+        }
+      },
+    })
     .use(rehypeStringify)
     .use(rehypeFormat)
     .use(rehypeAddClasses, {
